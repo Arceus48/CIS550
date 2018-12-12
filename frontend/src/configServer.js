40ad891013e0d7c8b5ec016d2a03644f6753fce9
Garatee <liao8631472@gmail.com>
2018-12-11 23:14:07 -0500

    change config

--- frontend/src/configServer.js
+++ frontend/src/configServer.js
@@ -1,6 +1,6 @@
 const configServer = {
-    // url: 'http://ec2-18-212-153-96.compute-1.amazonaws.com:3000/',
-    url: 'http://localhost:3001/'
+    url: 'http://ec2-18-212-153-96.compute-1.amazonaws.com:3000/',
+    // url: 'http://localhost:3001/'
 }
 
 
